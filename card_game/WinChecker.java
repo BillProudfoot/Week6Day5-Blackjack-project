10583289109e5c86a3c6459990714c4059fc2c7e
Bill Proudfoot <user@CODECLAN031.local>
2017-04-16 15:40:43 +0100

    Added win logic errors down from 22 to 16

--- card_game/WinChecker.java
+++ card_game/WinChecker.java
@@ -5,4 +5,17 @@ import java.util.*;
 
 public class WinChecker {
 
+  public void getwhoWins(player1.card1, player2.card2){
+  if (player1.card1 > player2.card2);
+
+  System.out.println("Keith wins");
+  else 
+    if (player1.card1 == player2card2);
+
+      System.out.println("The game is a Draw");
+  else
+    if (player2.card2 > player1.card1);
+
+      System.outprintln("Craig wins"); 
+}
 }
--- card_tests/WinCheckerTest.java
+++ card_tests/WinCheckerTest.java
@@ -6,5 +6,24 @@ import java.util.*;
 
 public class WinCheckerTest {
 
+  Player player1;
+  Player player2;
+  Card card1;
+  Card card2;
+
+  @Before
+  public void before(){
+    player1 = new Player("Keith");
+    player2 = new Player("Craig");
+    card1 = new Card(2, Suit.Clubs);
+    card2 = new Card(3, Suit.Spades);
+    player1.setHand(card1);
+    player2.setHand(card2);
+  }
+
+@Test
+public void whoWins(){
+  assertEquals("Craig wins", player1.card1());
+}
 
 }
