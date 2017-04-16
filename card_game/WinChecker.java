3eeb130a1c15671aa8c1f44a32f4558d868340df
Bill Proudfoot <user@CODECLAN031.local>
2017-04-16 15:51:09 +0100

    down to 10 errors

--- card_game/WinChecker.java
+++ card_game/WinChecker.java
@@ -5,16 +5,15 @@ import java.util.*;
 
 public class WinChecker {
 
-  public void getwhoWins(player1.card1, player2.card2){
-  if (player1.card1 > player2.card2);
+  public void getwhoWins(){
+  if (player1.card1() > player2.card2());
 
   System.out.println("Keith wins");
-  else 
-    if (player1.card1 == player2card2);
+  elsif (player1.card1() == player2card2());
 
       System.out.println("The game is a Draw");
-  else
-    if (player2.card2 > player1.card1);
+  elsif
+    (player2.card2() > player1.card1());
 
       System.outprintln("Craig wins"); 
 }
